86c6b4c0e513a283c007cad3060b0a6a95b0917e
Nejc Trdin <nejc.trdin@gmail.com>
2015-08-19 11:57:03 +0200

    Added a new example and rearanged the positioning when errors occur

--- content.py
+++ content.py
@@ -14,11 +14,12 @@ _GPL = ("Copyright (C) 2015  Nejc Trdin\n\n"
         "You should have received a copy of the GNU General Public License "
         "along with this program.  If not, see <http://www.gnu.org/licenses/>.")
 
-_ABOUT = "This is an application which enables the users to input a DEXi type function, and produces the values of first partial derivatives in the function's defined points.\nThe functions is defined in three lines: the first line gives the integer output for the corresponding sorted input values. The inputs are sorted firstly by the first attribute, then second, etc. The second line of the function gives the names of the input attributes, delimited by a comma. The last line gives the sizes of the respective input's scales, which are also integral.\nYou can check the code and possibly contribute at <a href=\"https://github.com/nejctrdin/DEXiFunctionDerivatives\">github</a>."
+_ABOUT = "This is an application which enables the users to input a DEXi type function, and produces the values of first partial derivatives in the function's defined points.\nThe functions is defined in three lines: the first line gives the integer output for the corresponding sorted input values. The inputs are sorted firstly by the first attribute, then second, etc. The second line of the function gives the names of the input attributes, delimited by a comma. The last line gives the sizes of the respective input's scales, which are also integral.\nFor exact syntax check the examples above.\nYou can check the code and possibly contribute at <a href=\"https://github.com/nejctrdin/DEXiFunctionDerivatives\">github</a>."
 
 _EXAMPLE1 = ("Linear in one variable", "012 first 3")
 _EXAMPLE2 = ("Linear in two variables", "012123234 first,second 3,3")
 _EXAMPLE3 = ("Monotone function", "000000001000111122112333344 first,second,third 3,3,3")
+_EXAMPLE4 = ("Random function", "423120124321201212013024421 first,second,third 3,3,3")
 
 
-_EXAMPLES = [_EXAMPLE1, _EXAMPLE2, _EXAMPLE3]
+_EXAMPLES = [_EXAMPLE1, _EXAMPLE2, _EXAMPLE3, _EXAMPLE4]
