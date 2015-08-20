9b4efa268d7112af49935109f159e68bf1c689e8
Nejc Trdin <nejc.trdin@gmail.com>
2015-08-20 12:30:43 +0200

    Removed unused imports and added code comments

--- server.py
+++ server.py
@@ -1,11 +1,6 @@
 from flask import Flask
 from flask import request
-from flask import session
-from flask import redirect
-from flask import url_for
-from flask import abort
 from flask import render_template
-from flask import flash
 from dexi import parse_function
 from dexi import get_derivatives
 from dexi import _format_number
@@ -19,36 +14,57 @@ app.jinja_env.lstrip_blocks = True
 
 @app.route("/")
 def index():
+    # serving index page with title, subtitle, about, examples and license
     entries = {"title": content._TITLE,
                "subtitle": content._SUBTITLE,
                "about": content._ABOUT,
                "examples": content._EXAMPLES,
                "license": content._GPL
     }
+    # render the page with a template
     return render_template("index.html", entries=entries)
 
 @app.route("/get_derivatives", methods=["POST"])
 def derivatives():
+    # the path which serves post requests for creating derivatives
     raw_function = ""
     if "names" in request.form:
+        # if "names" is in the request, it means it was called from the already
+        # created derivatives - the user changed the inputs and retried the
+        # derivative creation
         values = {}
+        # we read the fields in the request and parse out those that start with a v
         for field in request.form:
             if field[0] == "v":
+                # the values and the entries are put into a dictionary
+                # before that the keys have "v"s removed and cast to int
                 tmp_f = int(field.replace("v",""))
                 values[tmp_f] = request.form[field]
 
         value_list = []
+        # we sort the keys of the dictionary, to get the initial order
+        # and put the values into a value list
         for field in sorted(values):
             value_list.append(values[field])
 
+        # function is described in three lines
         function_description = []
+        # output values joined, delimited with a comma
         function_description.append(",".join(value_list))
+        # names are delimited by a comma (already in the request)
         function_description.append(request.form["names"])
+        # multiplicities are delimited by a comma (already in the request)
         function_description.append(request.form["multiplicity"])
+
+        # the raw function is represented as a new-line delimitied string
         raw_function = str("\n".join(function_description))
+
     elif "function" in request.form:
+        # if function is present in request, we read it directly
+        # parsing is done afterwards
         raw_function = request.form["function"]
     else:
+        # if neither of them are present, we return error status
         entries = {"raw_function": raw_function,
                    "ok": False,
                    "message": "Unsupported operation!",
@@ -57,10 +73,18 @@ def derivatives():
                    "about": content._ABOUT,
                    "license": content._GPL,
         }
+        # render the derivatives page
         return render_template("derivatives.html", entries=entries)
 
+    # we are interested in the time that took for generating the derivatives,
+    # evaluations and images
     t = time()
+
+    # we parse the function, getting the actual function, arguments, needed evaluations
+    # success status and possible message
     function, arguments, evaluations, success, message = parse_function(raw_function)
+
+    # variables are prepared for filling in - these are defaults if anything goes wrong
     raw_function = raw_function.replace("\r", "")
     raw_function = raw_function.split("\n")
     derivatives = None
@@ -70,12 +94,14 @@ def derivatives():
     names = ""
     image = ""
     if success:
+        # if the previous step succeeded we can construct the function, get derivatives,
+        # evaluations and the possible image
         names = ",".join(arguments)
         multiplicities = raw_function[2]
-    if success:
         derivatives, evaluations, image, success1, message1 = get_derivatives(function, evaluations)
         print "Query needed {0}s to execute!".format(_format_number(time() - t))
 
+    # form a dictionary to return to the user
     entries = {"raw_function": raw_function,
                "ok": success and success1,
                "message": " ".join([message, message1]),
@@ -92,14 +118,19 @@ def derivatives():
                "image": image
     }
 
+    # the derivatives page is rendered
     return render_template("derivatives.html", entries=entries)
 
 
 if __name__ == "__main__":
+    # parse arguments
     if len(argv) == 2:
+        # if license must be displayed
         if argv[1] == "license":
             print content._GPL
         else:
+            # otherwise treat the argument as port number
             app.run(port=int(argv[1]))
     else:
-        app.run(debug=True)
+        # otherwise just run the app on the app-decided port
+        app.run()
