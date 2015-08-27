Added derivatives to the tabelaric function and renamed dexiTest to test. Also fixed travis.yml for that reason. Moved out the image path from dexi into content.

dexi.py
@@ -11,6 +11,7 @@ from scipy.misc import derivative
 import random
 import string
 import os
+import content
 
 # different error statuses given back to the user in case of problems
 NOT_CORRECT_LINES = "Function should be represented in at least 3 lines!"
@@ -23,8 +24,6 @@ FUNCTION_OUTPUT_FLOAT = "All function outputs should be floats!"
 FUNCTION_EVALUATIONS_FLOAT = "All function evaluations should be floats {0}!"
 PROBLEM_DERIVATIVES = "There was a problem constructing derivatives!"
 NOT_CORRECT_ARGUMENTS_EVAL = "Number of function arguments ({0}) does not match number of supplied evaluation arguments ({1}) - {2}."
-
-DEFAULT_IMAGE_PATH = "static/images/"
 
 def parse_function(f_rep):
     # the function that parses the input string
@@ -185,7 +184,7 @@ def _scipy_derivatives(function, req_evaluations, arguments, output_image=True):
 
     # create the filename for the possible image
     image_file_name = ""
-    image_dir = DEFAULT_IMAGE_PATH
+    image_dir = content._DEFAULT_IMAGE_PATH
 
     if input_size < 3:
         # we create a list of possible characters that form the file names
@@ -217,8 +216,8 @@ def _scipy_derivatives(function, req_evaluations, arguments, output_image=True):
             # otherwise we have a 3D image
             fig = plt.figure()
             ax = fig.gca(projection="3d")
-            X = np.arange(-1, max_values[0] + 1.1, 0.1)
-            Y = np.arange(-1, max_values[1] + 1.1, 0.1)
+            X = np.arange(-0.5, max_values[0] + 0.6, 0.1)
+            Y = np.arange(-0.5, max_values[1] + 0.6, 0.1)
             ax.set_xlabel(arguments[0])
             ax.set_ylabel(arguments[1])
             ax.set_zlabel("Output")