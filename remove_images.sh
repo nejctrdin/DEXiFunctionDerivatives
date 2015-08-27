5d8f9c06e37242865896d180731539fc9df65e40
Nejc Trdin <nejc.trdin@gmail.com>
2015-08-27 15:45:54 +0200

    Added gif files to the removal script, and finding old enough files via find

--- remove_images.sh
+++ remove_images.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-for f in $(find DFD/static/images/ -name "*.png"); do
-    timestamp=$(stat $f | grep Access | grep -v Uid | cut -d" " -f2,3 | tr -d "-" | tr -d " " | tr -d ":" | cut -d "." -f1)
-    diff=$(( `date '+%Y%m%d%H%M%S'` - $timestamp ))
-    if [ $diff -gt 86400 ]
-    then
-        rm $f
-        echo "Removing file $f"
-    fi
+for f in $(find DFD/static/images/ -regex ".*\.\(png\|gif\)" -atime 1); do
+    rm $f
+    echo "Removing file $f"
 done
 
