35a2c49862551bcc41477d5fb245fd5082e4001d
Nejc Trdin <nejc.trdin@gmail.com>
2015-08-27 14:32:09 +0200

    Added correct path to images on pythonanywhere

--- remove_images.sh
+++ remove_images.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for f in $(find static/images/ -name "*.png"); do
+for f in $(find DFD/static/images/ -name "*.png"); do
     timestamp=$(stat $f | grep Access | grep -v Uid | cut -d" " -f2,3 | tr -d "-" | tr -d " " | tr -d ":" | cut -d "." -f1)
     diff=$(( `date '+%Y%m%d%H%M%S'` - $timestamp ))
     if [ $diff -gt 86400 ]
